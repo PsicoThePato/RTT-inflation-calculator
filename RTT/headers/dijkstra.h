a7de4f5e5358c55fe11d1d139a427db7f08d576c
Jairo <jairo.moutinho@edu.ufes.br>
2021-04-10 04:59:32 -0300

    :sparkles: algoritmo de dijsktra funcionando, mas o valgrind está com mais erro que minha vida

---
+++ RTT/headers/dijkstra.h
@@ -0,0 +1,10 @@
+#ifndef _DIJKSTRA_H_
+#define _DIJKSTRA_H_
+
+#include "graphOperations.h"
+#include "item.h"
+
+
+Item* dijkstraSP(Grafo* grafo, int source);
+
+#endif
---
+++ RTT/src/dijkstra.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../headers/dijkstra.h"
+#include "../headers/item.h"
+#include "../headers/PQ.h"
+
+
+void relax(Node* edge, PQ* pq, int from, double* distTo, Item* edgeTo)
+{
+    int to = edge->nodeLabel;
+    if(distTo[to] > distTo[from] + edge->weight)
+    {
+        distTo[to] = distTo[from] + edge->weight;
+        edgeTo[to].id = from;
+        edgeTo[to].value = distTo[to];
+        if(PQ_hasX(to, pq))
+        {
+            PQ_decrease_key(to, edge->weight, pq);
+            return;
+        }
+        Item node = {to, edge->weight};
+        PQ_insert(node, pq);
+    }
+}
+
+
+Item* dijkstraSP(Grafo* grafo, int source)
+{
+    Item *edgeTo = malloc(sizeof(Item) * grafo->nV);
+    double distTo[grafo->nV];
+    PQ* pq = PQ_init(grafo->nV);
+
+    for(int i=0; i<grafo->nV; i++)
+    {
+        distTo[i] = 99999.0;
+    }
+    distTo[source] = 0;
+    Item no_inical = {source, 0.0};
+    PQ_insert(no_inical, pq);
+
+    while(!PQ_empty(pq))
+    {
+        Item min_item = PQ_delmin(pq);
+        int min_node = min_item.id;
+        Node* node = grafo->adjList[min_node].head;
+        while(node)
+        {
+            relax(node, pq, min_node, distTo, edgeTo);
+            node = node->next;
+        }
+
+    }
+    PQ_finish(pq);
+    return edgeTo;
+}
