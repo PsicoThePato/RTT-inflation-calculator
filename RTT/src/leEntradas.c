3e71d20da52cdef68630fa300dd9d6f3ff40214a
Jairo <jairo.moutinho@edu.ufes.br>
2021-04-15 07:01:57 -0300

    :sparkles: rtt_aprox_calc function

--- RTT/src/leEntradas.c
+++ RTT/src/leEntradas.c
@@ -168,24 +168,29 @@ Item* rtt_aprox_calc(Grafo* graph)
  
     server_monitor = rtt_calc(graph, graph->servidores, graph->monitores, graph->nS, graph->nM);
     monitor_client = rtt_calc(graph, graph->monitores, graph->clients, graph->nM, graph->nC);
+    
     int aux = 0;
     int bigArrayRef = 0;
-    for(int i=0; i<graph->nS * graph->nM; i++)
+    double server_to_client = 999999.0;
+    for(int i=0; i<graph->nS; i+=graph->nM)
     {
-        for(int j=0; j<graph->nC; j++, bigArrayRef++)
+        for(int j=0; j<graph->nC; j++)
         {
-            end_rtt[bigArrayRef].from = server_monitor[i].from;
-            end_rtt[bigArrayRef].id = monitor_client[aux].id;
-            end_rtt[bigArrayRef].value = server_monitor[i].value + monitor_client[aux].value;
-            aux++;
-            if(aux == graph->nM * graph->nC)    
+            for(int k=0; k<graph->nM; k++)
             {
-                aux = 0;
+                if(server_monitor[i + k].value + monitor_client[k*graph->nC + j].value < server_to_client)
+                {   
+                    server_to_client = server_monitor[i + k].value + monitor_client[k*graph->nC + j].value;
+                    end_rtt[bigArrayRef].from = server_monitor[i + k].from;
+                    end_rtt[bigArrayRef].id = monitor_client[k*graph->nC + j].id;
+                    end_rtt[bigArrayRef].value = server_to_client;
+                }
             }
         }
     }
 
-    for(int i=0; i<graph->nC * graph->nS * graph->nM;i++)
+
+    for(int i=0; i<graph->nC * graph->nS; i++)
     {
         printf("A distancia do node %d para o node %d eh %lf\n", end_rtt[i].from, end_rtt[i].id, end_rtt[i].value);
     }
@@ -199,15 +204,13 @@ int main()
 {
     Grafo* graph = inicializaGrafo();
     //Item* edgeTo = dijkstraSP(graph, 0);
-    /*
-    Item* real_rtt_calc = rtt_calc(graph);
+    Item* real_rtt_calc = rtt_calc(graph, graph->servidores, graph->clients, graph->nS, graph->nC);
     for(int i=0; i<graph->nC * graph ->nS;i++)
     {
-        printf("A distancia para o node %d eh %lf\n", real_rtt_calc[i].id, real_rtt_calc[i].value);
+        printf("A distancia do node %d para o node %d eh %lf\n", real_rtt_calc[i].from, real_rtt_calc[i].id, real_rtt_calc[i].value);
     }
     free(real_rtt_calc);
-    */
-    Item* real_rtt_calc = rtt_aprox_calc(graph);
+    Item* real_rtt_calc1 = rtt_aprox_calc(graph);
     //free(edgeTo);
     destroiGrafo(graph);
 }
